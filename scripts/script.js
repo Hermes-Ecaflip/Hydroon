b3ab0cf1184480c4f29e1ab5015876569b308184
Ângelo Passos <angelocks22@gmail.com>
2024-07-11 00:08:23 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,12 @@ window.addEventListener('load', function() {
     const overlay = document.querySelector('.loading-overlay');
     overlay.style.display = 'none'; // Esconde o overlay após o carregamento
 });
+
+// Função para avançar o carrossel automaticamente a cada 7 segundos
+function avancarSlide() {
+    const proximoBotao = document.querySelector('[data-bs-slide="next"]');
+    proximoBotao.click();
+}
+
+// Iniciar o carrossel automaticamente
+setInterval(avancarSlide, 5000); // 7000 milissegundos = 7 segundos
